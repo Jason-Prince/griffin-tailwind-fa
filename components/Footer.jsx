29eabe694dd1e1586025cb6f5269aa372458045b
Jason Prince <jasoncprince@gmail.com>
2022-01-18 09:36:21 -0700

    footer: added fonts and responsive

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,23 +1,26 @@
+import { faEnvelope, faMapMarker, faPhone } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="grid grid-cols-12 col-start-1 col-end-13 p-6 nav:text-xl bg-dark">
-      <h3 className="col-start-1 col-end-13 pb-6 text-center text-white nav:text-4xl font-Roboto">
+    <div className="grid grid-cols-12 gap-4 p-4 text-xl nav:text-2xl bg-dark font-Roboto text-white whitespace-nowrap">
+      <h3 className="col-start-1 col-span-full justify-self-center text-3xl nav:text-4xl font-Montserrat ">
         Contact
       </h3>
-      <p className="col-start-1 col-end-13 text-center text-white font-Roboto">
-        801.872.9786
-      </p>
-      <p className="col-start-1 col-end-13 text-center text-white font-Roboto">
-        info@griffinec.com
-      </p>
-      <p className="col-start-1 col-end-13 text-center text-white font-Roboto">
-        303 N 350 W
-      </p>
-      <p className="col-start-1 col-end-13 text-center text-white font-Roboto">
-        Bountiful, UT 84010
-      </p>
+      <div className="col-start-1 col-span-full justify-self-center nav:col-start-1 nav:col-end-5 flex items-center gap-3">
+        <FontAwesomeIcon icon={faPhone} />
+        <p className="col-start-6">801.872.9786</p>
+      </div>
+      <div className="col-start-1 col-span-full justify-self-center nav:col-start-5 nav:col-end-8 flex items-center gap-3">
+        <FontAwesomeIcon icon={faEnvelope} />
+        <p className="col-start-6">info@griffinec.com</p>
+      </div>
+      <div className="col-start-1 col-span-full justify-self-center nav:col-start-8 nav:col-end-13 flex items-center gap-3">
+        <FontAwesomeIcon icon={faMapMarker} />
+        <p className="col-start-6">303 N 350 W Bountiful, UT 84010</p>
+      </div>
     </div>
   )
 }
