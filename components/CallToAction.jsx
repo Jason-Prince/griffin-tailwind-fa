4542e778622c5454c342af4e2f6ddd82b4121ebd
Jason Prince <jasoncprince@gmail.com>
2022-01-18 17:57:07 -0700

    made home page

--- components/CallToAction.jsx
+++ components/CallToAction.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const CallToAction = ({ text }) => {
   return (
-    <div className="grid grid-cols-12 text-xl text-center font-Montserrat nav:text-4xl">
-      <h4 className="self-center col-start-3 col-end-11 py-2 border-2 border-solid border-dark">
+    <div className="text-xl text-center w-80 font-Montserrat nav:text-4xl">
+      <h4 className="self-center px-2 py-2 border-2 border-solid border-dark">
         {text}
       </h4>
     </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,7 @@
+import CallToAction from '../components/CallToAction'
+import Footer from '../components/Footer'
+import Image from 'next/image'
+import Link from 'next/link'
 import Navbar from '../components/Navbar'
 import Services from './services'
 
@@ -5,6 +9,36 @@ export default function index() {
   return (
     <>
       <Navbar />
+      <div
+        className="w-full h-1/2 bg-cover bg-center bg-fixed"
+        style={{ backgroundImage: "url('/gec_banner_bg.jpg')" }}
+      >
+        <Image
+          src="/gec_banner_white.png"
+          width={4001}
+          height={1651}
+          alt="Griffin Energy Consulting"
+          layout="intrinsic"
+          className="backdrop-blur-sm backdrop-brightness-50 scale-90"
+        />
+      </div>
+      <div className="w-full h-1/2">
+        <Image
+          src="/home_page.jpg"
+          width={1897}
+          height={988}
+          alt="Helping you save the world one building at a time. Save money. Save energy. Save the planet."
+          layout="responsive"
+        />
+      </div>
+      <div className="grid justify-center my-14 cursor-pointer">
+        <Link href="/contact">
+          <a>
+            <CallToAction text={'Start your project today'} />
+          </a>
+        </Link>
+      </div>
+      <Footer />
     </>
   )
 }
