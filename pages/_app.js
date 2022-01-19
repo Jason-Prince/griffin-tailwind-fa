3328ce807901757485411fc0c95c7d45a09bcc38
Jason Prince <jasoncprince@gmail.com>
2022-01-18 19:22:17 -0700

    fixed fontawesome and favicon conflict

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,6 @@ import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-      </Head>
       <Component {...pageProps} />
     </>
   )
