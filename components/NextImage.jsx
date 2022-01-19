a9433b0fe5c429625c51df8694368580b6983b9e
Jason Prince <jasoncprince@gmail.com>
2022-01-18 19:47:49 -0700

    favicon is messing up other images... took it out.

--- components/NextImage.jsx
+++ components/NextImage.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 
 const NextImage = ({ image }) => {
   const { alt, src, width, height, layout } = image
-  return <Image src={src} width={width} height={height} layout={layout} alt={alt} />
+  return (
+    <Image
+      src={src}
+      width={width}
+      height={height}
+      layout={layout}
+      alt={alt}
+      priority
+    />
+  )
 }
 
 export default NextImage
